3a1af7f0007cb336da89d0d97eb0be9a017121ff
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-08 07:59:23 +0530

    yes me

--- err.js
+++ err.js
@@ -1,4 +1,4 @@
-// # yes we can use true 
+// * yes we can use true as mathematical expression 1
 // console.log(true + true + true /3);
 let arr = [34, 56, 3, 2, 65, 45];
 for (let i = 0; i < 5; i++) {
