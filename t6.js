1701be0a73fbf36ed9b907eb19f90dfb3a71b77f
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-10 13:40:49 +0530

    yes me

--- t6.js
+++ t6.js
@@ -1,8 +1,15 @@
 // console.log( null  && true && false);
-let passIt = () => {
-    return console.log('this is the callback function');
-}
-let highOrder = (a) => {
-    console.log(a());
-}
-highOrder(passIt);
+// let passIt = () => {
+//     return console.log('this is the callback function');
+// }
+// let highOrder = (a) => {
+//     console.log(a());
+// }
+// highOrder(passIt);
+// let curryIng = (a) => {
+//     return function (b) {
+//         return a + b;
+//     }
+
+// }
+// console.log(curryIng(2)(6));
