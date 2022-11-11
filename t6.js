a01cafd94a8214a2ceedeaed13ce9ec1d0c35a06
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-11 06:41:49 +0530

    yes me

--- t6.js
+++ t6.js
@@ -13,6 +13,10 @@
 
 // }
 // console.log(curryIng(2)(6));
-let a = [23,34,213,32,352,'mango','nanna'];
+// let a = [23,34,213,32,352,'mango','nanna'];
 // ! slice range act like this [ )
-console.log(a.slice(1,4));
+// console.log(a.slice(1,4));
+let a = [23,32,12,34,43];
+let b = a.filter((item)=> item === 12 );
+// let b = a.map((item)=> item === 12 );
+console.log(b);
