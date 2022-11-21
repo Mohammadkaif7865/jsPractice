0a91d44daa37f4ec0850d3dcee8aef2622d1c2ac
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-21 08:37:53 +0530

    random learing

--- t7.js
+++ t7.js
@@ -1,4 +1,5 @@
 let a = 5;
 let b = 5;
+let random = Math.floor(Math.random() * 10000);
 // ? you can not directly use variable name to declare the variable
-console.log(a + b);
+console.log(a + b + random);
