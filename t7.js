5e2e596d9d18c3a4f62467370afe3b31f704c82c
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-23 20:14:57 +0530

    yes em

--- t7.js
+++ t7.js
@@ -1,8 +1,13 @@
-let a = [23,234, 43, 2, 435, 43, -1, 323, 323,34534];
-let element = a[0];
-for (let i = 0; i < a.length - 1; i++) {
-    if (a[i + 1] > element) {
-        element = a[i + 1];
-    }
+// let a = [23,234, 43, 2, 435, 43, -1, 323, 323,34534];
+// let element = a[0];
+// for (let i = 0; i < a.length - 1; i++) {
+//     if (a[i + 1] > element) {
+//         element = a[i + 1];
+//     }
+// }
+
+// console.log(element);
+for (var i = 0; i <= 6; ++i) {
+    console.log(i);
+    i = i + 1;
 }
-console.log(element);
