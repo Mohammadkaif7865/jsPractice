e587fa6fea11686c0cb944b15ac3c03635b0ac17
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-23 21:54:45 +0530

    yes me

--- t9.js
+++ t9.js
@@ -1,3 +1,12 @@
 let arr = ['ghi@hotmail.com', 'def@yahoo.com', 'ghi@gmail.com', 'abc@channelier.com', 'abc@hotmail.com', 'def@hotmail.com', 'abc@gmail.com', 'abc@yahoo.com', 'def@channelier.com', 'jkl@hotmail.com', 'ghi@yahoo.com', 'def@gmail.com'];
 arr.sort();
-console.log(arr);
+for (let i = 0; i < arr.length-1; i++) {
+    if (arr[i].split("@")[0] === arr[i+1].split("@")[0]) {
+        console.log(true);
+    }
+    if (arr[i].split("@")[0] !== arr[i+1].split("@")[0]) {
+        console.log(false);
+    }
+    
+}
+// console.log(arr[i].split("@")[0], arr[i].split("@")[1].split("."));
