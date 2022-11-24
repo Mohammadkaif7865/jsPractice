f11af59b181bae07cdbbf92acf3ecdb6a3ef23c9
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-24 11:19:50 +0530

    this is intership code

--- t9.js
+++ t9.js
@@ -1,12 +1,45 @@
-let arr = ['ghi@hotmail.com', 'def@yahoo.com', 'ghi@gmail.com', 'abc@channelier.com', 'abc@hotmail.com', 'def@hotmail.com', 'abc@gmail.com', 'abc@yahoo.com', 'def@channelier.com', 'jkl@hotmail.com', 'ghi@yahoo.com', 'def@gmail.com'];
+let arr = ['ghi@hotmail.com', 'def@yahoo.com', 'ghi@gmail.com', 'abc@channelier.com', 'abc@hotmail.com', 'def@hotmail.com', 'abc@gmail.com', 'abc@yahoo.com', 'def@channelier.com', 'jkl@hotmail.com', 'ghi@yahoo.com', 'jkl@yahoo.com', 'def@gmail.com'];
 arr.sort();
-for (let i = 0; i < arr.length-1; i++) {
-    if (arr[i].split("@")[0] === arr[i+1].split("@")[0]) {
-        console.log(true);
+let i = 0;
+while (i < arr.length - 2) {
+    if (arr[i].split("@")[0] === arr[i + 3].split("@")[0]) {
+
+        let temp1 = arr[i + 1];
+        let temp2 = arr[i + 2];
+        let temp3 = arr[i + 3];
+        arr[i + 1] = temp3;
+        arr[i + 2] = temp1;
+        arr[i + 3] = temp2;
+        i += 4;
     }
-    if (arr[i].split("@")[0] !== arr[i+1].split("@")[0]) {
-        console.log(false);
+    else if (arr[i].split("@")[0] === arr[i + 2].split("@")[0]) {
+        if (arr[i].split("@")[1].split(".")[0] !== "channelier") {
+            let temp1 = arr[i];
+            let temp2 = arr[i + 1];
+            let temp3 = arr[i + 2];
+            arr[i] = temp3;
+            arr[i + 1] = temp1;
+            arr[i + 2] = temp2;
+        }
+        else if (arr[i].split("@")[1].split(".")[0] === "channelier" && arr[i + 2].split("@")[1].split(".")[0] === "yahoo") {
+            let temp = arr[i + 1];
+            arr[i + 1] = arr[i + 2];
+            arr[i + 2] = temp;
+        }
+        i += 3;
+        console.log(arr.length - 2, i);
     }
-    
+    else if (arr[i].split("@")[0] === arr[i + 1].split("@")[0]) {
+
+        if (arr[i].split("@")[1].split(".")[0] !== "channelier" && arr[i + 1].split("@")[1].split(".")[0] === "yahoo") {
+            let temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+
+        i += 2;
+    }
+    else i++;
+
 }
-// console.log(arr[i].split("@")[0], arr[i].split("@")[1].split("."));
+console.log(arr);
